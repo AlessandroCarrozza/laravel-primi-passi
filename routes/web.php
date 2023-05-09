aec6a1c1b4dcb6703b55b30648944376ddfc7fd4
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-05-09 16:36:45 +0200

    Stampa di Hello World

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $title = "Hello World";
+
+    $texts = [
+        "title" => $title,
+    ];
+
+    return view('home', $texts);
 });
